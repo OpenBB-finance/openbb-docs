cf324b2d53685008735ecf96c0e58bdbe279fca4
Meghan Lopes <88618738+Meg1211@users.noreply.github.com>
2024-11-17 18:25:34 -0500

    Documentation Changes (#38)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,122 +1,172 @@
 import Link from "@docusaurus/Link";
 import Layout from "@theme/Layout";
 import clsx from "clsx";
-import ChevronRightIcon from "../components/Icons/ChevronRight";
 import DarkBlueRadialGradient from "../components/Icons/RadialGradients/DarkBlue";
-import RubyRedRadialGradient from "../components/Icons/RadialGradients/RubyRed";
+import PlatformIcon from "../components/Icons/Platform";
+import OpenBBTerm from "../components/Icons/OpenBBTerm";
+import Excel from "../components/Icons/Excel";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import ThemedImage from "@theme/ThemedImage";
+import useBaseUrl from "@docusaurus/useBaseUrl";
 
 export default function Home(): JSX.Element {
-	return (
-		<Layout description="Documentation for free and open source OpenBB products.">
-			<article className="min-h-[75vh] w-full relative py-20 md:py-10 flex flex-col items-center justify-center overflow-hidden">
-				<DarkBlueRadialGradient className="absolute opacity-40 -left-[1000px] w-[1600px] h-[1600px] lg:-left-[1000px] lg:opacity-40 -top-20 xl:-top-60 xl:-left-[800px] 2xl:opacity-50" />
-				<RubyRedRadialGradient className="absolute opacity-40 -right-[1000px] w-[1600px] h-[1600px] lg:-right-[1000px] lg:opacity-40 -top-20 xl:-top-60 xl:-right-[800px] 2xl:opacity-50" />
-				<div className="flex mx-auto flex-col items-center justify-center w-full max-w-[1100px]">
-					<div className="z-10 flex flex-col items-center justify-center w-full h-full mb-10 md:mb-0">
-						<h1 className="mx-6 font-bold text-2xl lg:text-3xl xl:text-4xl leading-10 text-center mb-4 tracking-widest uppercase">
-							OpenBB Documentation
-						</h1>
-						<p className="mx-6 w-[315px] md:w-[880px] text-sm lg:text-lg xl:text-xl text-center text-grey-900 dark:text-white">
-							All the documentation for the tools you need for your investment
-							research.
-						</p>
-					</div>
-					<div className="flex flex-col md:flex-row w-full container items-center justify-center gap-6 px-6 sm:mx-0 my-8 lg:!my-10">
-						<Link
-							style={{
-								backgroundSize: "100% 110%",
-							}}
-							to="/terminal"
-							className="!bg-grey-900 bg-[url('/img/pro.png')] bg-no-repeat shadow-sm group !no-underline text-white dark:hover:border-white hover:border-grey-600/80 relative w-full h-[238px] max-w-full p-8 rounded flex flex-col items-start justify-start border border-grey-300 hover:!text-white"
-						>
-							<h3 className="uppercase tracking-widest font-bold mb-0 mt-0 text-white">
-								OpenBB Terminal
-							</h3>
-							<p className="text-sm lg:text-base dark:text-grey-300">
-								The OpenBB Terminal is an AI-powered research and analytics
-								workspace.
-							</p>
-							<p
-								className={clsx(
-									"mt-auto inline-flex items-center gap-1 font-normal text-sm",
-								)}
-							>
-								See more
-								<ChevronRightIcon className="group-hover:translate-x-2 tw-transition w-3" />
-							</p>
-						</Link>
-					</div>
-					<div className="my-4 md:my-0" />
+  const {
+    siteConfig: {
+      themeConfig: { colorMode: { defaultMode } = { defaultMode: "light" } },
+    },
+  } = useDocusaurusContext() as {
+    siteConfig: { themeConfig: { colorMode?: { defaultMode: string } } };
+  };
 
-					<div className="flex flex-col md:flex-row w-full container items-center justify-center gap-6 px-6 sm:mx-0 sm:flex-col">
-						<Link
-							style={{
-								backgroundSize: "100% 130%",
-							}}
-							to="/excel"
-							className="!bg-grey-900 bg-[url('/img/excelbg.png')] bg-no-repeat shadow-sm group !no-underline text-white dark:hover:border-white hover:border-grey-600/80 relative w-full h-[238px] max-w-full p-8 rounded flex flex-col items-start justify-start border border-grey-300 hover:!text-white"
-						>
-							<h3 className="uppercase tracking-widest font-bold mb-0 mt-0 text-white">
-								OpenBB Add-In for Excel
-							</h3>
-							<p className="text-sm lg:text-base text-grey-300">
-								The OpenBB Add-In for Excel allows access to the same data as
-								the OpenBB Terminal Pro, but through Excel.
-							</p>
-							<p
-								className={clsx(
-									"mt-auto inline-flex items-center gap-1 font-normal text-sm",
-								)}
-							>
-								See more
-								<ChevronRightIcon className="group-hover:translate-x-2 tw-transition w-3" />
-							</p>
-						</Link>
-						<Link
-							style={{
-								backgroundSize: "100% 130%",
-							}}
-							to="/platform"
-							className="!bg-grey-900 bg-[url('/img/sdk_bg.png')] bg-no-repeat shadow-sm group !no-underline text-white dark:hover:border-white hover:border-grey-600/80 relative w-full h-[238px] max-w-full p-8 rounded flex flex-col items-start justify-start border border-grey-300 hover:!text-white"
-						>
-							<h3 className="uppercase tracking-widest font-bold mb-0 mt-0 text-white">
-								OpenBB Platform
-							</h3>
-							<p className="text-sm lg:text-base dark:text-grey-300">
-								The OpenBB Platform provides a convenient way to access raw
-								financial data from multiple data providers.
-							</p>
-							<p
-								className={clsx(
-									"mt-auto inline-flex items-center gap-1 font-normal text-sm",
-								)}
-							>
-								See more
-								<ChevronRightIcon className="group-hover:translate-x-2 tw-transition w-3" />
-							</p>
-						</Link>
-					</div>
+  return (
+    <Layout description="Documentation for free and open source OpenBB products.">
+      <article className="min-h-[75vh] w-full relative py-20 md:py-10 flex flex-col items-center justify-center overflow-hidden">
+        <DarkBlueRadialGradient className="absolute opacity-40 -left-[1000px] w-[1600px] h-[1600px] lg:-left-[1000px] lg:opacity-30 top-1/2 -translate-y-1/2 xl:-left-[800px] 2xl:opacity-40" />
+        <div className="flex mx-auto flex-col items-center justify-center w-full max-w-[1100px]">
+          <div className="z-10 flex flex-col items-center justify-center w-full mb-10 md:mb-0">
+            <ThemedImage
+              sources={{
+                light: useBaseUrl("/img/logo-dark.png"),
+                dark: useBaseUrl("/img/logo-light.png"),
+              }}
+              alt="OpenBB Logo"
+              className="mb-6 w-[200px] md:w-[250px] !shadow-none !border-none"
+            />
+            <p className="mx-6 text-base text-center text-[#2A2A2A] dark:text-white max-w-[315px] md:max-w-[880px]">
+              All the documentation for the tools you need for your investment
+              research.
+            </p>
+          </div>
+          <div className="flex flex-col md:flex-row w-full container items-center justify-center gap-6 px-6 sm:mx-0 my-8 lg:!my-10">
+            <Link
+              style={{ backgroundSize: "100% 130%" }}
+              to="/platform"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="!bg-white dark:!bg-[#151518] bg-no-repeat shadow-sm group !no-underline text-white relative w-full h-[240px] max-w-full p-8 rounded flex flex-col items-start justify-start overflow-hidden"
+            >
+              <div
+                className="absolute inset-0 bg-gradient-radial from-orange-600 dark:from-orange-700 opacity-40 z-[1]"
+                style={{
+                  background:
+                    "radial-gradient(circle at top right, var(--tw-gradient-from), var(--tw-gradient-to))",
+                }}
+              />
+              <div className="relative z-[2]">
+                <h3 className="uppercase text-lg tracking-wider font-bold mb-0 mt-0 text-[#2A2A2A] dark:text-white flex items-center gap-2">
+                  <div className="dark:bg-[#36363F] bg-[#EBEBED] shadow-sm p-1.5 rounded">
+                    <PlatformIcon className="w-4 h-4" />
+                  </div>
+                  OpenBB Platform
+                </h3>
+                <p className="text-base mt-2 text-[#434344] dark:text-grey-100">
+                  The OpenBB Platform provides a convenient way to access raw
+                  financial data from multiple data providers.
+                </p>
+                <p
+                  className={clsx(
+                    "mt-auto inline-flex items-center gap-1 font-normal text-[#070707] dark:text-gray-200 dark:hover:text-white text-sm dark:bg-[#36363F] bg-[#EBEBED] hover:bg-[#DADBDE] dark:hover:bg-[#2A2A31] px-4 py-2 rounded-md transition-colors"
+                  )}
+                >
+                  See more
+                </p>
+              </div>
+            </Link>
+            <Link
+              style={{ backgroundSize: "100% 130%" }}
+              to="/terminal"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="!bg-white dark:!bg-[#151518] bg-no-repeat shadow-sm group !no-underline text-white relative w-full h-[240px] max-w-full p-8 rounded flex flex-col items-start justify-start overflow-hidden"
+            >
+              <div
+                className="absolute inset-0 bg-gradient-radial from-[#0097DE] opacity-40 z-[1]"
+                style={{
+                  background:
+                    "radial-gradient(circle at top right, var(--tw-gradient-from), var(--tw-gradient-to))",
+                }}
+              />
+              <div className="relative z-[2]">
+                <h3 className="uppercase text-lg tracking-wider font-bold mb-0 mt-0 text-[#2A2A2A] dark:text-white flex items-center gap-2">
+                  <div className="dark:bg-[#36363F] bg-[#EBEBED] shadow-sm p-1.5 rounded">
+                    <OpenBBTerm className="w-5 h-5" />
+                  </div>
+                  OpenBB
+                </h3>
+                <p className="text-base mt-2 text-[#434344] dark:text-grey-100">
+                  OpenBB provides investors with a reinvented interface to
+                  perform investment research optimised for efficiency.
+                </p>
+                <p
+                  className={clsx(
+                    "mt-auto inline-flex items-center gap-1 font-normal text-[#070707] dark:text-gray-200 dark:hover:text-white text-sm dark:bg-[#36363F] bg-[#EBEBED] hover:bg-[#DADBDE] dark:hover:bg-[#2A2A31] px-4 py-2 rounded-md transition-colors"
+                  )}
+                >
+                  See more
+                </p>
+              </div>
+            </Link>
+            <Link
+              style={{ backgroundSize: "100% 130%" }}
+              to="/excel"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="!bg-white dark:!bg-[#151518] bg-no-repeat shadow-sm group !no-underline text-white relative w-full h-[240px] max-w-full p-8 rounded flex flex-col items-start justify-start overflow-hidden"
+            >
+              <div
+                className="absolute inset-0 bg-gradient-radial from-[#15803D] opacity-40 z-[1]"
+                style={{
+                  background:
+                    "radial-gradient(circle at top right, var(--tw-gradient-from), var(--tw-gradient-to))",
+                }}
+              />
+              <div className="relative z-[2]">
+                <h3 className="uppercase text-lg tracking-wider font-bold mb-0 mt-0 text-[#2A2A2A] dark:text-white flex items-center gap-2">
+                  <div className="dark:bg-[#36363F] bg-[#EBEBED] shadow-sm p-1.5 rounded">
+                    <Excel className="w-5 h-5" />
+                  </div>
+                  OpenBB excel add-in
+                </h3>
+                <p className="text-base mt-2 text-[#434344] dark:text-grey-100">
+                  Our Excel Add-In seamlessly fetches financial data, so you can
+                  focus on making investment decisions.
+                </p>
+                <p
+                  className={clsx(
+                    "mt-auto inline-flex items-center gap-1 font-normal text-[#070707] dark:text-gray-200 dark:hover:text-white text-sm dark:bg-[#36363F] bg-[#EBEBED] hover:bg-[#DADBDE] dark:hover:bg-[#2A2A31] px-4 py-2 rounded-md transition-colors"
+                  )}
+                >
+                  See more
+                </p>
+              </div>
+            </Link>
+          </div>
+          <div className="my-4 md:my-0" />
 
-					<div className="my-2 md:my-0" />
-					<p className="z-20">
-						Our website can be found{" "}
-						<a
-							href="https://openbb.co"
-							className="text-burgundy-300 !underline hover:text-burgundy-400 active:text-burgundy-500"
-						>
-							here
-						</a>{" "}
-						and OpenBB Bot documentation can be found {" "}
-						<a
-							href="https://docs.openbb.co/bot"
-							className="text-burgundy-300 !underline hover:text-burgundy-400 active:text-burgundy-500"
-						>
-							here
-						</a>
-						.
-					</p>
-				</div>
-			</article>
-		</Layout>
-	);
+          <div className="my-2 md:my-0" />
+          <p className="z-20 text-base text-[#2A2A2A] dark:text-grey-100 text-center md:text-left px-6">
+            Our website can be found{" "}
+            <a
+              href="https://openbb.co"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-[#33BBFF] !underline hover:text-[#99DDFF] active:text-[#0088CC]"
+            >
+              here
+            </a>{" "}
+            and OpenBB Bot documentation can be found{" "}
+            <a
+              href="https://docs.openbb.co/bot"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-[#33BBFF] !underline hover:text-[#99DDFF] active:text-[#0088CC]"
+            >
+              here
+            </a>
+            .
+          </p>
+        </div>
+      </article>
+    </Layout>
+  );
 }
--- src/theme/SearchBar/index.js
+++ src/theme/SearchBar/index.js
@@ -7,8 +7,8 @@ import Translate from "@docusaurus/Translate";
 import { useHistory, useLocation } from "@docusaurus/router";
 import { isRegexpStringMatch } from "@docusaurus/theme-common";
 import {
-	useAlgoliaContextualFacetFilters,
-	useSearchResultUrlProcessor,
+  useAlgoliaContextualFacetFilters,
+  useSearchResultUrlProcessor,
 } from "@docusaurus/theme-search-algolia/client";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import translations from "@theme/SearchTranslations";
@@ -19,202 +19,202 @@ import { useIFrameContext } from "../Root.tsx";
 let DocSearchModal = null;
 
 function Hit({ hit, children }) {
-	return <Link to={hit.url}>{children}</Link>;
+  return <Link to={hit.url}>{children}</Link>;
 }
 
 function ResultsFooter({ state, onClose }) {
-	return (
-		<Link to={`/search?q=${state.query}`} onClick={onClose}>
-			<Translate
-				id="theme.SearchBar.seeAll"
-				values={{ count: state.context.nbHits }}
-			>
-				{"See all {count} results"}
-			</Translate>
-		</Link>
-	);
+  return (
+    <Link to={`/search?q=${state.query}`} onClick={onClose}>
+      <Translate
+        id="theme.SearchBar.seeAll"
+        values={{ count: state.context.nbHits }}
+      >
+        {"See all {count} results"}
+      </Translate>
+    </Link>
+  );
 }
 
 function mergeFacetFilters(f1, f2) {
-	const normalize = (f) => (typeof f === "string" ? [f] : f);
-	return [...normalize(f1), ...normalize(f2)];
+  const normalize = (f) => (typeof f === "string" ? [f] : f);
+  return [...normalize(f1), ...normalize(f2)];
 }
 
 function DocSearch({ contextualSearch, externalUrlRegex, ...props }) {
-	const location = useLocation();
-	const { siteMetadata } = useDocusaurusContext();
-	const processSearchResultUrl = useSearchResultUrlProcessor();
-	const contextualSearchFacetFilters = useAlgoliaContextualFacetFilters();
-	const configFacetFilters = props.searchParameters?.facetFilters ?? [];
-	const facetFilters = contextualSearch
-		? // Merge contextual search filters with config filters
-			mergeFacetFilters(contextualSearchFacetFilters, configFacetFilters)
-		: // ... or use config facetFilters
-			configFacetFilters;
-	// We let user override default searchParameters if she wants to
-	const searchParameters = {
-		...props.searchParameters,
-		facetFilters,
-	};
-	const history = useHistory();
-	const searchContainer = useRef(null);
-	const searchButtonRef = useRef(null);
-	const [isOpen, setIsOpen] = useState(false);
-	const [initialQuery, setInitialQuery] = useState(undefined);
-	const importDocSearchModalIfNeeded = useCallback(() => {
-		if (DocSearchModal) {
-			return Promise.resolve();
-		}
-		return Promise.all([
-			import("@docsearch/react/modal"),
-			// import("@docsearch/react/style"),
-			import("./styles.css"),
-		]).then(([{ DocSearchModal: Modal }]) => {
-			DocSearchModal = Modal;
-		});
-	}, []);
-	const onOpen = useCallback(() => {
-		importDocSearchModalIfNeeded().then(() => {
-			searchContainer.current = document.createElement("div");
-			document.body.insertBefore(
-				searchContainer.current,
-				document.body.firstChild,
-			);
-			setIsOpen(true);
-		});
-	}, [importDocSearchModalIfNeeded, setIsOpen]);
-	const onClose = useCallback(() => {
-		setIsOpen(false);
-		searchContainer.current?.remove();
-	}, [setIsOpen]);
-	const onInput = useCallback(
-		(event) => {
-			importDocSearchModalIfNeeded().then(() => {
-				setIsOpen(true);
-				setInitialQuery(event.key);
-			});
-		},
-		[importDocSearchModalIfNeeded, setIsOpen, setInitialQuery],
-	);
-	const navigator = useRef({
-		navigate({ itemUrl }) {
-			// Algolia results could contain URL's from other domains which cannot
-			// be served through history and should navigate with window.location
-			if (isRegexpStringMatch(externalUrlRegex, itemUrl)) {
-				window.location.href = itemUrl;
-			} else {
-				history.push(itemUrl);
-			}
-		},
-	}).current;
-	const transformItems = useRef((items) =>
-		(props.transformItems
-			? // Custom transformItems
-				props.transformItems(items)
-			: // Default transformItems
-				items.map((item) => ({
-					...item,
-					url: processSearchResultUrl(item.url),
-				}))
-		).filter((item) => {
-			const firstPathSegment = location.pathname.split("/")[1];
-			return !firstPathSegment
-				? true
-				: item.url.startsWith(`/${firstPathSegment}/`);
-		}),
-	).current;
-	const resultsFooterComponent = useMemo(
-		() =>
-			// eslint-disable-next-line react/no-unstable-nested-components
-			(footerProps) => <ResultsFooter {...footerProps} onClose={onClose} />,
-		[onClose],
-	);
-	const transformSearchClient = useCallback(
-		(searchClient) => {
-			searchClient.addAlgoliaAgent(
-				"docusaurus",
-				siteMetadata.docusaurusVersion,
-			);
-			return searchClient;
-		},
-		[siteMetadata.docusaurusVersion],
-	);
-	useDocSearchKeyboardEvents({
-		isOpen,
-		onOpen,
-		onClose,
-		onInput,
-		searchButtonRef,
-	});
-	return (
-		<>
-			<Head>
-				{/* This hints the browser that the website will load data from Algolia,
+  const location = useLocation();
+  const { siteMetadata } = useDocusaurusContext();
+  const processSearchResultUrl = useSearchResultUrlProcessor();
+  const contextualSearchFacetFilters = useAlgoliaContextualFacetFilters();
+  const configFacetFilters = props.searchParameters?.facetFilters ?? [];
+  const facetFilters = contextualSearch
+    ? // Merge contextual search filters with config filters
+      mergeFacetFilters(contextualSearchFacetFilters, configFacetFilters)
+    : // ... or use config facetFilters
+      configFacetFilters;
+  // We let user override default searchParameters if she wants to
+  const searchParameters = {
+    ...props.searchParameters,
+    facetFilters,
+  };
+  const history = useHistory();
+  const searchContainer = useRef(null);
+  const searchButtonRef = useRef(null);
+  const [isOpen, setIsOpen] = useState(false);
+  const [initialQuery, setInitialQuery] = useState(undefined);
+  const importDocSearchModalIfNeeded = useCallback(() => {
+    if (DocSearchModal) {
+      return Promise.resolve();
+    }
+    return Promise.all([
+      import("@docsearch/react/modal"),
+      import("./styles.css"),
+    ]).then(([{ DocSearchModal: Modal }]) => {
+      DocSearchModal = Modal;
+    });
+  }, []);
+  const onOpen = useCallback(() => {
+    importDocSearchModalIfNeeded().then(() => {
+      searchContainer.current = document.createElement("div");
+      document.body.insertBefore(
+        searchContainer.current,
+        document.body.firstChild
+      );
+      setIsOpen(true);
+    });
+  }, [importDocSearchModalIfNeeded]);
+  const onClose = useCallback(() => {
+    setIsOpen(false);
+    searchContainer.current?.remove();
+  }, []);
+  const onInput = useCallback(
+    (event) => {
+      importDocSearchModalIfNeeded().then(() => {
+        setIsOpen(true);
+        setInitialQuery(event.key);
+      });
+    },
+    [importDocSearchModalIfNeeded] // Removed unnecessary dependencies
+  );
+  const navigator = useRef({
+    navigate({ itemUrl }) {
+      // Algolia results could contain URL's from other domains which cannot
+      // be served through history and should navigate with window.location
+      if (isRegexpStringMatch(externalUrlRegex, itemUrl)) {
+        window.location.href = itemUrl;
+      } else {
+        history.push(itemUrl);
+      }
+    },
+  }).current;
+  const transformItems = useRef((items) =>
+    (props.transformItems
+      ? // Custom transformItems
+        props.transformItems(items)
+      : // Default transformItems
+        items.map((item) => ({
+          ...item,
+          url: processSearchResultUrl(item.url),
+        }))
+    ).filter((item) => {
+      const firstPathSegment = location.pathname.split("/")[1];
+      return !firstPathSegment
+        ? true
+        : item.url.startsWith(`/${firstPathSegment}/`);
+    })
+  ).current;
+  const resultsFooterComponent = useMemo(
+    () =>
+      // eslint-disable-next-line react/no-unstable-nested-components
+      (footerProps) =>
+        <ResultsFooter {...footerProps} onClose={onClose} />,
+    [onClose]
+  );
+  const transformSearchClient = useCallback(
+    (searchClient) => {
+      searchClient.addAlgoliaAgent(
+        "docusaurus",
+        siteMetadata.docusaurusVersion
+      );
+      return searchClient;
+    },
+    [siteMetadata.docusaurusVersion]
+  );
+  useDocSearchKeyboardEvents({
+    isOpen,
+    onOpen,
+    onClose,
+    onInput,
+    searchButtonRef,
+  });
+  return (
+    <>
+      <Head>
+        {/* This hints the browser that the website will load data from Algolia,
         and allows it to preconnect to the DocSearch cluster. It makes the first
         query faster, especially on mobile. */}
-				<link
-					rel="preconnect"
-					href={`https://${props.appId}-dsn.algolia.net`}
-					crossOrigin="anonymous"
-				/>
-			</Head>
+        <link
+          rel="preconnect"
+          href={`https://${props.appId}-dsn.algolia.net`}
+          crossOrigin="anonymous"
+        />
+      </Head>
 
-			<DocSearchButton
-				onTouchStart={importDocSearchModalIfNeeded}
-				onFocus={importDocSearchModalIfNeeded}
-				onMouseOver={importDocSearchModalIfNeeded}
-				onClick={onOpen}
-				ref={searchButtonRef}
-				translations={translations.button}
-			/>
+      <DocSearchButton
+        onTouchStart={importDocSearchModalIfNeeded}
+        onFocus={importDocSearchModalIfNeeded}
+        onMouseOver={importDocSearchModalIfNeeded}
+        onClick={onOpen}
+        ref={searchButtonRef}
+        translations={translations.button}
+      />
 
-			<BrowserOnly>
-				{() =>
-					isOpen &&
-					DocSearchModal &&
-					searchContainer.current &&
-					createPortal(
-						<DocSearchModal
-							onClose={onClose}
-							initialScrollY={window.scrollY}
-							initialQuery={initialQuery}
-							navigator={navigator}
-							transformItems={transformItems}
-							hitComponent={Hit}
-							transformSearchClient={transformSearchClient}
-							{...(props.searchPagePath && {
-								resultsFooterComponent,
-							})}
-							{...props}
-							searchParameters={searchParameters}
-							placeholder={translations.placeholder}
-							translations={translations.modal}
-						/>,
-						searchContainer.current,
-					)
-				}
-			</BrowserOnly>
-		</>
-	);
+      <BrowserOnly>
+        {() =>
+          isOpen &&
+          DocSearchModal &&
+          searchContainer.current &&
+          createPortal(
+            <DocSearchModal
+              onClose={onClose}
+              initialScrollY={window.scrollY}
+              initialQuery={initialQuery}
+              navigator={navigator}
+              transformItems={transformItems}
+              hitComponent={Hit}
+              transformSearchClient={transformSearchClient}
+              {...(props.searchPagePath && {
+                resultsFooterComponent,
+              })}
+              {...props}
+              searchParameters={searchParameters}
+              placeholder={translations.placeholder}
+              translations={translations.modal}
+            />,
+            searchContainer.current
+          )
+        }
+      </BrowserOnly>
+    </>
+  );
 }
 
 export default function SearchBar() {
-	const { siteConfig } = useDocusaurusContext();
-	const { isIFrame } = useIFrameContext();
-	return (
-		<div className="flex gap-2">
-			<DocSearch {...siteConfig.themeConfig.algolia} />
-			{!isIFrame && (
-				<a
-					target="_blank"
-					rel="noreferrer noopener"
-					href="https://pro.openbb.co/"
-					className="_btn-dark text-xs hover:text-black"
-				>
-					<span className="md:hidden">Login</span>
-					<span className="hidden md:inline">Login</span>
-				</a>
-			)}
-		</div>
-	);
+  const { siteConfig } = useDocusaurusContext();
+  const { isIFrame } = useIFrameContext();
+  return (
+    <div className="flex gap-2">
+      <DocSearch {...siteConfig.themeConfig.algolia} />
+      {!isIFrame && (
+        <a
+          target="_blank"
+          rel="noreferrer noopener"
+          href="https://pro.openbb.co/"
+          className="_login-button text-sm" // Changed from _btn-dark to _login-button
+        >
+          <span className="md:hidden">Login</span>
+          <span className="hidden md:inline">Login</span>
+        </a>
+      )}
+    </div>
+  );
 }
